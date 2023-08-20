e9b5abfa2bfc021ca5ccc237cf0b9918fc8fdaa3
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-20 18:56:13 -0500

    Updated Model

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNet_EF_CRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_EF_CRUD.Models
 {
     public class Dish
     {
+        [Key]
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Chef { get; set; }
+
+        public int Tastiness { get; set; }
+
+        public int Calories { get; set; }
+
+        public String Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set;} = DateTime.Now;
     }
 }
